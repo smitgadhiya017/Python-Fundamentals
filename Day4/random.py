1696bf7e463ebed8d696ba6c555fe600be23ec12
smitgadhiya017 <smitgadhiya3484@gmail.com>
2026-03-27 20:20:25 +0530

    Random function,List and Rock-Paper-Scissors game

---
+++ Day4/random.py
@@ -0,0 +1,19 @@
+# Random integer number find karva mate
+import random
+
+# random_integer = random.randint(1,10)
+# print(random_integer)
+
+# # Floating number mate 0 and 1 ni vache na j
+# random_number_0_to_1 = random.random()
+# print(random_number_0_to_1)
+#
+# random_float = random.uniform(1,10)
+# print(random_float)
+
+# Random Heads and Tails
+random_coin= random.randint(0,1)
+if random_coin == 0 :
+    print("Heads")
+elif random_coin == 1:
+    print("Tails")
