4970a8e5c940fe7f1b623ec1ec5e5f18eaa8a9e9
smitgadhiya017 <smitgadhiya3484@gmail.com>
2026-03-29 18:13:14 +0530

    First commit

---
+++ Day6/task.py
@@ -0,0 +1,30 @@
+# Function Practice
+def my_function():
+    print("Hello")
+    print("bye")
+
+my_function()
+
+# While loop
+
+# Any other work and Final project Done in
+# "Reeborg's World" website in "Hurdle 1,2,3" for Practice and "Maze" for final project
+
+# This is final project code. where all function is in-built in "Reeborg's World" website
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+# while front_is_clear():
+#     move()
+# turn_left()
+#
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         turn_left()
